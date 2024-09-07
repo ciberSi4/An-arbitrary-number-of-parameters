2524ca60349c51a78d35c502b651691d1cc0338a
ciberSi4 <alex.baikonur@mail.ru>
2024-09-07 08:03:46 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,14 @@
+def single_root_words (root_word, *other_words):
+    same_words : list = []
+
+    for i in range(len(other_words)):
+        if (root_word.lower() in other_words[i].lower()) or (other_words[i].lower() in root_word.lower()):
+            same_words += [str(other_words[i])]
+
+    return same_words
+
+
+result1 = single_root_words("rich", "richiest", "orichalcum", "cheers", "richies")
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
